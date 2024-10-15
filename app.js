c4cc482592b588093d6cb30ec3e29666c3df3e06
SalmanHossen <hossainsalman873@gmail.com>
2024-10-15 08:26:30 +0600

    basic app.js pre require setup

--- app.js
+++ app.js
@@ -2,6 +2,25 @@
 const express=require('express');
 const cors=require('cors');
 const helmet=require('helmet');
-
+const hpp=require('hpp');
 const rateLimit=require('express-rate-limit');
-const bodyParser=require('body-parser')
+const bodyParser=require('body-parser');
+
+const app=express();
+
+
+//Middleware Setup
+app.use(cors());
+app.use(helmet())
+app.use(hpp());
+app.use(bodyParser.json())
+
+const rateLimiter=rateLimit({
+   window:15*60*1000,
+   max:100
+})
+app.use(rateLimiter)
+
+
+
+module.exports=app;
