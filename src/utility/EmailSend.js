52e73303fbc1497bb02c86e8969b02276f3900e5
SalmanHossen <hossainsalman873@gmail.com>
2024-10-15 20:26:29 +0600

    remove the .env file

--- src/utility/EmailSend.js
+++ src/utility/EmailSend.js
@@ -1,13 +1,30 @@
 
+//Load environment variables from .env file
+require('dotenv').config();
 const nodemailer=require('nodemailer');
+
 
 const SendEmailUtility=async(EmailTo,EmailText,EmailSubject)=>{
 
-   let transporter=nodemailer.createTransport({\
-
+   let transporter=nodemailer.createTransport({
+      host:process.env.EMAIL_HOST,
+      port:process.env.EMAIL_PORT,
+      auth:{
+         user:"process.env.EMAIL_HOST",
+         pass:`process.env.EMAIL_PASS`
+      },tls:{
+         rejectUnauthorized:false
+      }
    });
 
    let mailOption={
-      
+      form:process.env.EMAIL_FROM,,
+      to:EmailTo,
+      subject:EmailSubject,
+      text:EmailText
    }
+
+   return await transporter.sendMail(mailOption);
 }
+
+module.exports=SendEmailUtility;
