2af3e13b4c3d53e39d17204fcbc4d342061f189f
SalmanHossen <hossainsalman873@gmail.com>
2024-10-15 08:44:38 +0600

    add multer and nodemailer packages

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 
+
+//file require
+const router=require('./src/route/Api');
+
+
+
+//package import
 const express=require('express');
-const cors=require('cors');
+const app=new express();
+
 const helmet=require('helmet');
 const hpp=require('hpp');
-const rateLimit=require('express-rate-limit');
 const bodyParser=require('body-parser');
+const rateLimit=require('express-rate-limit');
+const cors=require('cors');
 
-const app=express();
 
 
 //Middleware Setup
 app.use(cors());
-app.use(helmet())
+app.use(helmet());
 app.use(hpp());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const rateLimiter=rateLimit({
    window:15*60*1000,
