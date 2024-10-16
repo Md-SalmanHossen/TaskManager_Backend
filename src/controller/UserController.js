ce45193075edca3c0c63618b3a67cd44513403cf
SalmanHossen <hossainsalman873@gmail.com>
2024-10-16 14:04:19 +0600

    user login success

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,5 +1,7 @@
 //model import
 const UsersModel=require('../model/UsersModel');
+const jwt=require('jsonwebtoken');
+
 
 exports.registration=async(req,res)=>{
    try {
@@ -14,7 +16,27 @@ exports.registration=async(req,res)=>{
 
 
 exports.login=async(req,res)=>{
-   
+   try {
+      
+      let reqBody=req.body;
+      let userCount=await UsersModel.find(reqBody);
+      
+      if(userCount.length>0){
+
+         //jwt token
+         let PayLoad={exp:Math.floor(Date.now()/1000)+(24*60*60),data:reqBody['email']}
+         let token=jwt.sign(PayLoad,"1234-xyz");
+         res.json({status:"success",message:"User Found",token:token})
+
+      }else{ 
+         res.json({status:"fail",message:"No User Found"})
+      }
+
+      res.json({status:"success",message:userCount});
+
+   } catch (error) {
+      
+   }
 }
 
 exports.profileUpdate=async(req,res)=>{
